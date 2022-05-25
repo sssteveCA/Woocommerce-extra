4856b1d10f0ec7d4c5737adcfeb4a1beead20cc5
Acer <Acer@DESKTOP-K09QF87>
2022-05-25 19:18:32 +0200

    Updated woocommerce-extra.php
    Edited we_activation function

--- interfaces/constants.php
+++ interfaces/constants.php
@@ -5,7 +5,11 @@ namespace WoocommerceExtra\Interfaces;
 //Generic constants
 interface Constants{
 
+    //Directories
+    const DIR_PLUGIN = "/woocommerce-extra";
+
     //Files
     const FILE_LOG = "log.txt";
+
 }
 ?>
--- woocommerce-extra.php
+++ woocommerce-extra.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: Woocommerce extra
  * Description: addons functions for Woocommerce plugin
@@ -11,11 +10,18 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+require_once('interfaces/constants.php');
+
+use WoocommerceExtra\Interfaces\Constants as C;
+
+$logDir = plugin_dir_url(__FILE__).C::FILE_LOG;
+
 register_activation_hook(__FILE__,'we_activation');
 function we_activation(){
+    global $logDir;
     if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
         // Yes, WooCommerce is enabled
-        
+        file_put_contents($logDir,"Woocommerce attivo\r\n",FILE_APPEND);     
     } else {
         // WooCommerce is NOT enabled!
     }
