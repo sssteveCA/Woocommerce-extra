334f74e4a19f46ae8596bb5d621f0ec2a558a520
Acer <Acer@DESKTOP-K09QF87>
2022-05-25 18:58:04 +0200

    First commit of Woocommerce extra plugin

---
+++ woocommerce-extra.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Woocommerce extra
+ * Description: addons functions for Woocommerce plugin
+ * Version: 1.10.3
+ * Requires at least: 5.0
+ * Requires PHP: 7.4
+ * Author: Stefano Puggioni
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+?>
