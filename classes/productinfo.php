1b1c055ad96d3f0df5cadd4ee2688581d70e3b75
Acer <Acer@DESKTOP-K09QF87>
2022-05-26 22:56:19 +0200

    Added interfaces/productinfoerrors.php

--- classes/productinfo.php
+++ classes/productinfo.php
@@ -2,17 +2,36 @@
 
 namespace WoocommerceExtra\Classes;
 
+use WoocommerceExtra\Interfaces\ProductInfoErrors as Pie;
+use WoocommerceExtra\Interfaces\Constants as C;
+
 //Generate HTML table that contains info list of a product
-class ProductInfo{
+class ProductInfo implements Pie,C{
     private string $path; //Filesystem path of the json file
     private string $content; //Entire file content
+    private string $logFile; //Filesystem path of log file
 
-    public function __construct()
+    public function __construct(array $data)
     {
-        
+        $this->logFile = isset($data['logFile']) ? $data['logFile'] : C::FILE_LOG;
+        $this->path = isset($data['path'])? $data['path']: null;
+        if(!$this->path)throw new \Exception(Pie::PATHNOTSPECIFIED_EXC);
+        if(!$this->jsonFile())throw new \Exception(Pie::INVALIDTYPE_EXC);
     }
 
     public function getPath(): string{return $this->path;}
     public function getContent(): string{return $this->content;}
+
+    //Check if file is JSON type
+    private function jsonFile(): bool{
+        $json = false;
+        $type = mime_content_type($this->path);
+        file_put_contents($this->logFile,"Mime => {$type}\r\n",FILE_APPEND);
+        if($type == 'application/json'){
+            //File type is JSON
+            $json = true;
+        }//if($type == 'application/json'){
+        return $json;
+    }
 }
 ?>
---
+++ interfaces/productinfoerrors.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WoocommerceExtra\Interfaces;
+
+interface ProductInfoErrors{
+    const INVALIDTYPE_EXC = "Il tipo di file specificato non è valido";
+    const PATHNOTSPECIFIED_EXC = "Non è stato specificato un file da aprire";
+}
+?>
