e33e03d67f346c9667181c78658c5d199e1318d8
Acer <Acer@DESKTOP-K09QF87>
2022-05-27 09:43:06 +0200

    Updated productinfo.php
    Updated genHtml method

--- classes/productinfo.php
+++ classes/productinfo.php
@@ -10,6 +10,7 @@ class ProductInfo implements Pie,C{
     private string $path; //Filesystem path of the json file
     private string $content; //Entire file content
     private array $arrayContent; //JSON string decoded into array
+    private string $html; //HTML generated
     private string $logFile; //Filesystem path of log file
 
     public function __construct(array $data)
@@ -25,6 +26,7 @@ class ProductInfo implements Pie,C{
     public function getPath(): string{return $this->path;}
     public function getArrayContent(): array{return $this->arrayContent;}
     public function getContent(): string{return $this->content;}
+    public function getHtml(): string{return $this->html;}
 
     //Check if specified file exists
     private function exists(): bool{
@@ -39,10 +41,34 @@ class ProductInfo implements Pie,C{
 
     //Generate HTML table from array
     private function genHtml(): bool{
-        $gen = false;
+        $gen = true;
         $this->arrayContent = json_decode($this->content,true);
         file_put_contents($this->logFile,"Array content => ".var_export($this->arrayContent,true)."\r\n",FILE_APPEND);
-        $gen = true;
+        $this->html = "<h2>Descrizione</h2>";
+        foreach($this->arrayContent as $table){
+            $thead = isset($table['thead']); //Check if $table has 'thead' key with a set value
+            $tbody = (is_array($table['tbody']) && !empty($table['body'])); //Check if tbody property is an array and if it's not empty
+            if($thead && $tbody){
+                //Correct format
+                $this->html .= '<div><table class="table">';
+                $this->html .= '<thead><tr>';
+                $this->html .= '<th colspan="2">'.$table['thead'].'</th>';
+                $this->html .= '</tr></thead>';
+                $this->html .= '<tbody>';
+                foreach($table['tbody'] as $key => $value){
+                    $this->html .= '<tr>';
+                    $this->html .= '<th scope="row">'.$key.'</th>';
+                    $this->html .= '<td>'.$value.'</td>';
+                    $this->html .= '</tr>';
+                }//foreach($table['tbody'] as $key => $value){
+                $this->html .= '</tbody>';
+                $this->html .= '</table></div>';
+            }//if($thead && $tbody){
+            else{
+                $gen = false;
+                break;
+            }        
+        }//foreach($this->arrayContent as $table){
         return $gen;
     }
 
